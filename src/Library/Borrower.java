0144dada66450868e3efa621262332eb49faacfb
m3d8 <m3d8@lin25.ugrad.cs.ubc.ca>
2014-03-26 20:22:48 -0700

    Comments in SQL

--- src/Library/Borrower.java
+++ src/Library/Borrower.java
@@ -15,7 +15,11 @@ public class Borrower {
 	public void search(String keyword){
 		Statement  stmt;
 		ResultSet  rs;
-		//stub
+
+		// SELECT * FROM book, hassubject WHERE book.book_title = keyword OR book.book_mainAuthor = keyword OR hassubject.hassubject_subject = keyword, book.book_callNumber = hassubject.book_callNumber ORDER BY book.book_callNumber AS Temp
+		// SELECT COUNT (bookcopy) FROM bookcopy, Temp WHERE book.bookcopy_status = "in", Temp.book_callNumber = bookcopy.book_callNumber ORDER BY bookcopy.book_callNumber
+		// SELECT COUNT (bookcopy) FROM bookcopy, Temp WHERE book.bookcopy_status = "out", Temp.book_callNumber = bookcopy.book_callNumber ORDER BY bookcopy.book_callNumber
+		// 
 	}
 
 	//	Check
@@ -28,16 +32,24 @@ public class Borrower {
 
 	public void checkAccount(int id){
 		//stub
+		// SELECT borrowing FROM borrowing WHERE borrowing.borrowing_inDate IS NULL AND borrowing.borrowing_outDate IS NOT NULL AND borrowing.borrower_bid = id AS Temp
+		// this should return the books they borrowed
+		// SELECT * FROM Fine, borrowing, Temp WHERE fine.fine_paidDate IS NULL AND borrowing.borrowing_borid = fine.borrowing_borid AND borrowing.borrower_bid = id
+		// This shoudl return all the fines that have not been paid from the list of borrowed books?
+		// SELECT * FROM holdrequest WHERE holdrequest.borrower_bid = id
 	}
 	//	Place a hold request for a book that is out. When the item is r
 	//	eturned, the system sends an
 	//	email to the borrower and informs the library clerk to keep the book out of the shelves.
 
-	public void placeHoldRequest(int callNo){
+	public void placeHoldRequest(int callNo, int id){
 		//stub
+		// INSERT INTO holdrequest VALUES (?, id, callNo, currentDate)
+		// first value is unique generated integer, currentDate is obv
 	}
 	//Pay a fine - not sure what to do here. 
-	public void payFine(){
+	public void payFine(int fid){
 		//stub
+		// UPDATE fine SET fine.fine_paidDate = currentDate WHERE fine.fine_fid = fid
 	}
 }
